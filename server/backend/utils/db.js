db2f8a891057ded58992d0729b09a59a281b8586
veeran <veeranveeran605@gmail.com>
2025-10-23 14:06:17 +0530

    Fixed MySQL connection and updated backend

--- server/backend/utils/db.js
+++ server/backend/utils/db.js
@@ -1,19 +1,21 @@
-import mysql from 'mysql'
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employeems"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-con.connect(function(err) {
-    if(err) {
-        console.log("connection error")
-    } else {
-        console.log("Connected")
-    }
-})
+con.connect(function (err) {
+  if (err) {
+    console.log("Database connection error:", err);
+  } else {
+    console.log("✅ Connected to MySQL Database");
+  }
+});
 
 export default con;
-
